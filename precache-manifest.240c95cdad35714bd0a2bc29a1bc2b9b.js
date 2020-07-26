3f9d559d968c0b1ca55cb3d036ed8a85c4313a42
mdlaras <52792426+mdlaras@users.noreply.github.com>
2020-07-26 12:11:38 +0700

    Updates

---
+++ precache-manifest.240c95cdad35714bd0a2bc29a1bc2b9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b55b24ec20a34aa0f5057ecaad1d39a5",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9f6a994f4ffa423e9f43",
+    "url": "/robofriends/static/css/main.3f09ac48.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f6a994f4ffa423e9f43",
+    "url": "/robofriends/static/js/main.ae2dc871.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
